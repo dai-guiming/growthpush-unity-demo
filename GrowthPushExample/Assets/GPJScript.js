d2333f0438c916d9a3432a4ad5fa220d8ecea585
asahi_daiki <asahi@sirok.co.jp>
2013-12-14 01:01:16 +0900

    Add Sample Project

---
+++ GrowthPushExample/Assets/GPJScript.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var applicationId : int = 0;
+var secret : String = "secret";
+var environment : GrowthPush.Environment = GrowthPush.Environment.development;
+var option : GrowthPush.Option = GrowthPush.Option.All;
+var debug : boolean = true;
+var senderId : String = "senderId";
+
+function Awake () {
+	GrowthPush.initialize(applicationId, secret, environment, option, debug);
+	GrowthPush.register(senderId);
+}
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI () {
+		if(GUI.Button(new Rect(10, 10, 100, 100), "tap button")) {
+			GrowthPush.trackEvent("tap button");
+		}
+
+		if(GUI.Button(new Rect(10, 120, 100, 100), "setTag tappbutton")) {
+			GrowthPush.setTag("tap button");
+		}
+	}
---
+++ GrowthPushExample/Assets/GPScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GPScript  : MonoBehaviour {
+
+	public int applicationId = 0;
+	public string secret = "secret";
+	public bool debug = true;
+	public GrowthPush.Environment environment = GrowthPush.Environment.development;
+	public GrowthPush.Option option = GrowthPush.Option.All;
+	public string senderID = "senderID";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Awake () {
+		GrowthPush.initialize(applicationId, secret, environment, option, debug);
+		GrowthPush.register(senderID);
+
+		GrowthPush.launchWithNotification(data => {
+			object growthpushObj = null;
+			if(data.TryGetValue("growthpush", out growthpushObj)) {
+				object notificationId = null;
+				if((growthpushObj as Dictionary<string, object >).TryGetValue("notificationId", out notificationId)) {
+					GrowthPush.trackEvent("Launch via push notification " + notificationId);
+				}
+			}
+		});
+	}
+
+	void OnGUI () {
+		if(GUI.Button(new Rect(10, 10, 100, 100), "trackEvent launch!")) {
+			GrowthPush.trackEvent("launch!");
+		}
+
+		if(GUI.Button(new Rect(10, 120, 100, 100), "setTag launch")) {
+			GrowthPush.setTag("launch");
+		}
+	}
+}
